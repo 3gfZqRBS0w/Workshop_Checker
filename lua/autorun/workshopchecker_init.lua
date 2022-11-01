58fd5e766325744f7deba106f59d4493be4355b6
Lombrès <94228891+Lombrest@users.noreply.github.com>
2022-11-01 17:12:17 +0100

    A0.0.1

---
+++ lua/autorun/workshopchecker_init.lua
@@ -0,0 +1,29 @@
+local rep = "wc/"
+local file, folder = file.Find(rep.."*", "LUA")
+
+print([[
+	-------------------------------------------------------------
+	WC : WORKSHOP CHECKER 
+	created by Lombrès in 2022
+	-------------------------------------------------------------
+]])
+
+
+if (SERVER) then
+    for k, v in pairs(file) do
+        if (string.StartWith(v, "sv") and string.EndsWith(v, ".lua")) then
+            include(rep..v)
+        elseif (string.StartWith(v, 'sh') and string.EndsWith(v,".lua")) then
+            include(rep..v)
+            AddCSLuaFile(rep..v)
+        elseif (string.StartWith(v, "cl") and string.EndsWith(v, ".lua") ) then  
+			AddCSLuaFile(rep..v)
+		end
+    end
+else
+	for k, v in pairs(file) do
+		if ( string.StartWith(v, "cl") or string.StartWith(v, "sh") and string.EndsWith(v, ".lua") ) then
+			include(rep..v)
+		end
+	end
+end
